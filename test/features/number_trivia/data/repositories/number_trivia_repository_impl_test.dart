ae62493ef8ad35efdd7d5ea01c7cca9dbc79ed18
Sven Schroeder <schuen@gmx.de>
2020-11-01 20:37:13 +0100

    start test class for ntriviarep_impl

---
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,30 @@
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_loal_datasource.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class MockRemoteDatasource extends Mock
+    implements NumberTriviaRemoteDataSource {}
+
+class MockLocaDatasource extends Mock implements NumberTriviaLocalDatasource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  MockRemoteDatasource mockRemoteDatasource;
+  MockLocaDatasource mockLocaDatasource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockNetworkInfo = MockNetworkInfo();
+    mockLocaDatasource = MockLocaDatasource();
+    mockRemoteDatasource = MockRemoteDatasource();
+    repository = NumberTriviaRepositoryImpl();
+
+    
+  });
+}
