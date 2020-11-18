ecd692fa3b6cfa7731465ef18b352f8617223ced
Sven Schroeder <schuen@gmx.de>
2020-11-18 14:27:42 +0100

    finished part 9 local data source

--- test/features/number_trivia/data/datasources/number_trivia_local_datasource_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_datasource_test.dart
@@ -2,14 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/error/excepions.dart';
 import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_loal_datasource.dart';
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:matcher/matcher.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
 
 class MockSharedPreferences extends Mock implements SharedPreferences {}
-
 
 void main() {
   NumberTriviaLocalDataSourceImpl dataSource;
@@ -33,8 +34,31 @@ void main() {
       //act
       final result = await dataSource.getLastNumberTrivia();
       //assert
-      verify(mockSharedPreferences.getString('CACHED_NUMBER_TRIVIA'));
+      verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
       expect(result, equals(tNumberTriviaModel));
+    });
+
+    test(
+        'should throw a CachedException when there is no Number Trivia stored in the Shared Preferences',
+        () async {
+      //arrange
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      //act
+      final call = dataSource.getLastNumberTrivia;
+      //assert
+      expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+    });
+  });
+
+  group('cached number trivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel(number: 1, text: 'test trivia');
+    test('should call Shared Preferences to cache the data', () async {
+      //act
+      dataSource.cacheNumberTrivia(tNumberTriviaModel);
+      //assert
+      final String jsonTNTM = json.encode(tNumberTriviaModel.toJson());
+      verify(mockSharedPreferences.setString(CACHED_NUMBER_TRIVIA, jsonTNTM));
     });
   });
 }
